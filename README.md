d656bb1825355b07845c55cbf36a17d4250e16d5
ahardiag <ahardiag@gmail.com>
2024-04-22 15:51:23 +0200

    first example of use

--- README.md
+++ README.md
@@ -23,12 +23,50 @@ verdi code create core.code.installed
         --computer curie \
         --filepath-executable "/opt/raspa_v2.0.48/bin/simulate"
 ```
-Iddicate after `--filepath-executable` the executable path of RASA installed in the computer.
+Indicate after `--filepath-executable` the executable path of RASPA installed in the computer.
 
 ### Other dependies 
 ```
 pip install -r requirements.txt
 ```
 
+## Example
 
+### Run on CoRE MOF 2019
+- **Input structures** : `examples/subset-coremof/cif`
+    As an input, we provide a folder with 4140 cif files obtained by :
+    - downloading the dataset `coremof-2019` through [MOFXDB](https://github.com/snurr-group/mofdb-x-archive/tree/dc8a0295db) (12018 MOFs)
+    - converting CIF format using `openbabel`
+    - selecting only the cif files we already have charges calculated using [lsmo-epfl/EQeq](https://github.com/lsmo-epfl/EQeq) (4140 MOFs)
+    - correcting the name of the CIF file with `python check_and_convert_filenames.py`. `aiida-raspa` do not accept nonalphanumeric (exept underscore) in the filename (e.g. j.inoche.2015.06.036_inc06102-mmc1_clean_openbabel.cif).
+
+- **running the calculation**
+We used the following code (`verdi code show 1`):
+```
+-----------------------  -----------------------------------------
+PK                       1
+UUID                     e4afcb8d-4528-4561-a23e-c68226f2b8e5
+Type                     core.code.installed
+Computer                 curie (localhost), pk: 1
+Filepath executable      /opt/raspa_v2.0.48/bin/simulate
+Label                    raspa_run
+Description              Run a simulation with RASPA
+Default calc job plugin  raspa
+Use double quotes        False
+With mpi
+Prepend text             export RASPA_DIR=/opt/raspa_v2.0.48
+                         export DYLD_LIBRARY_PATH=${RASPA_DIR}/lib
+                         export LD_LIBRARY_PATH=${RASPA_DIR}/lib
+Append text
+-----------------------  -----------------------------------------
+```
+
+command to submit all jobs at once from the root directory:
+```bash
+verdi run eqeq_raspa_screening.py raspa_run \
+    --inputpath examples/subset-coremof/cif_converted/ \
+    --num_max 80 \
+    --verbose 
+```
+To test the submission on a few structure, one can pass the maximum number of submission with `--num_max`.
 
---
+++ check_and_convert_filenames.py
@@ -0,0 +1,70 @@
+import os
+import re
+import shutil
+
+def check_filenames(directory):
+    # Regex pattern to find non-alphanumeric and non-underscore characters
+    pattern = re.compile(r'[^a-zA-Z0-9_]')
+
+    # Variable to track if any invalid filenames are found
+    warning_flag = False
+    count_warn = 0
+
+    # Walk through the directory
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # Get the filename without the extension
+            filename_without_extension = os.path.splitext(file)[0]
+
+            # Search for non-alphanumeric or underscore characters
+            if pattern.search(filename_without_extension):
+                # Print a warning message with the filename
+                print(f"Warning: Invalid filename found - {file}")
+                warning_flag = True
+                count_warn+=1
+
+    # If no invalid filenames found, print all clear message
+    if not warning_flag:
+        print("All filenames are valid.")
+    return count_warn
+
+
+def copy_and_rename_directory(source_directory, target_directory_suffix='_converted'):
+    # Create the target directory with the specified suffix
+    target_directory = f"{source_directory}{target_directory_suffix}"
+    os.makedirs(target_directory, exist_ok=True)
+    print(target_directory)
+
+    # Regex pattern to replace non-alphanumeric or underscore characters
+    pattern = re.compile(r'[^a-zA-Z0-9_]')
+
+    count_modif = 0
+
+    # Iterate through all files in the source directory
+    for filename in os.listdir(source_directory):
+        # Split the filename into name and extension
+        file_base, file_extension = os.path.splitext(filename)
+        
+        # Replace non-alphanumeric or underscore characters in the filename part
+        sanitized_filename = pattern.sub('_', file_base) + file_extension
+
+        # Define source and target file paths
+        source_file = os.path.join(source_directory, filename)
+        target_file = os.path.join(target_directory, sanitized_filename)
+
+        # Copy file from source to target directory
+        shutil.copy(source_file, target_file)
+        count_modif+=1
+        print(f"Copied and renamed {filename} to {sanitized_filename}")
+
+    print(f'Number of filename converted in {source_directory+target_directory_suffix} : {count_modif}')
+
+# Directory to check
+source_directory = 'examples/subset-coremof/cif'
+
+# Check filenames
+count_warn = check_filenames(source_directory)
+print(f'Number of uncorrected formatted text files : {count_warn}')
+
+# Create new valid directory
+copy_and_rename_directory(source_directory)
